01c272bc455e153ce2c4844386860fa007a6df35
arnabuchiha <arnabkeya04@gmail.com>
2017-08-11 18:23:12 +0530

    second

---
+++ app/src/main/java/com/vitualsenseltd/arnab/chatterx/UserDetails.java
@@ -0,0 +1,15 @@
+package com.vitualsenseltd.arnab.chatterx;
+
+public class UserDetails {
+    public static String username = "";
+    static String password = "";
+    static String language= "";
+    static String chatWith = "";
+
+    public String getLanguage(){
+        return language;
+    }
+    public void setLanguage(String language){
+        this.language=language;
+    }
+}
