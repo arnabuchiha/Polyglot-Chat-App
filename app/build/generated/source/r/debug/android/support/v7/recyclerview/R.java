1cb37d2d1ce90efa4926722f5356d0380b8ad36e
arnabuchiha <arnabkeya04@gmail.com>
2017-08-11 19:47:08 +0530

    third

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -19,12 +19,12 @@ public final class R {
         public static final int stackFromEnd = 0x7f010118;
     }
     public static final class dimen {
-        public static final int fastscroll_default_thickness = 0x7f080077;
-        public static final int fastscroll_margin = 0x7f080078;
-        public static final int fastscroll_minimum_range = 0x7f080079;
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080081;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080082;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f080083;
+        public static final int fastscroll_default_thickness = 0x7f080079;
+        public static final int fastscroll_margin = 0x7f08007a;
+        public static final int fastscroll_minimum_range = 0x7f08007b;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080083;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080084;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f080085;
     }
     public static final class id {
         public static final int item_touch_helper_previous_elevation = 0x7f0d0005;
